ea2f8a7f04895162f8c60f70ce23d4bada782d2c
Injamul Hasan <injamulhasanakash@yahoo.com>
2026-07-01 01:46:32 +0600

    reviewed reserved keywords in C

---
+++ reservedkeywords.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int mai() {
+
+  return 0;
+}
+
+
+//https://beginnersbook.com/2014/01/c-keywords-reserved-words/
+
+//break – Used with any loop OR switch case.
+
+//int, float, char, double, long – These are the data types and used during variable declaration.
+
+//for, while, do – types of loop structures in C.
+
+//void – One of the return type.
+
+//goto – Used for redirecting the flow of execution.
+
+//auto, signed, const, extern, register, unsigned – defines a variable.
+
+//return – This keyword is used for returning a value.
+
+//continue – It is generally used with for, while and dowhile loops, when compiler encounters this statement it performs the next iteration of the loop, skipping rest of the statements of current iteration.
+
+//enum – Set of constants.
+
+//sizeof – It is used to know the size.
+
+//struct, typedef  – Both of these keywords used in structures (Grouping of data types in a single record).
+
+//union – It is a collection of variables, which shares the same memory location and memory storage.
