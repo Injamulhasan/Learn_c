50d8d28e41e6845fae847ea961f84b00ec330fcd
Injamul Hasan <injamulhasanakash@yahoo.com>
2026-06-30 13:17:04 +0600

    math operation ,\n \t difference

--- math.c
+++ math.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <time.h>
 
 int main()
 {
+printf("Hello \t,i'm Injamul\n");
+printf("Hello ,i'm Injamul\n");
+printf("Hello ,i'm Injamul\n");
+printf("Hello,\ni'm Injamul\n ");
 
-  return 0
+
+printf("%d\n",5+15);
+printf("%d\n",5-15);
+printf("%d",5*15);
+printf("%d",75/15);
+
+return 0;
 }
