35f9482164a6ca5421d5c441e78722b35e06de55
Injamul Hasan <injamulhasanakash@yahoo.com>
2026-07-01 23:57:48 +0600

    review input function

---
+++ input.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+int a,b;
+float c ,d;
+
+scanf("%d", &a);
+scanf("%d", &b);
+
+printf("%d %d", a,b);
+
+//printf("%d", b);
+
+
+
+scanf("%f %f", &c, &d);
+printf("%f\n %f",c,d);
+
+
+  return 0;
+}
