c169ddd21ae377c7fe0c344b146e49264ff5a589
Injamul Hasan <injamulhasanakash@yahoo.com>
2026-07-04 23:23:02 +0600

    project:1st calculator

---
+++ project_calculator.c
@@ -0,0 +1,26 @@
+#include <stdio.h>  // prepocessor
+
+int main() {  //function
+
+  float n1,n2,result;
+
+  printf ("Please Enter he Two Numbers(4 1.99):");
+  scanf("%f %f",&n1 ,&n2);
+
+  result = n1 + n2 ;
+  printf("%f + %f = %f\n", n1 ,n2 ,result);
+
+
+  result = n1 - n2 ;
+  printf("%f - %f = %f\n", n1 ,n2 ,result);
+
+
+  result = n1 * n2 ;
+  printf("%f * %f = %f\n", n1 ,n2 ,result);
+
+
+  result = n1 / n2 ;
+  printf("%f / %f = %f\n", n1 ,n2 ,result);
+
+  return 0;
+}
